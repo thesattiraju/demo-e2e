e10c4f3e36bddd9a4e2b6de034b6790c3b04ae4b
raghaves <raghaves@microsoft.com>
2020-07-09 20:20:40 +0530

    Policy Integration changes

--- .github/scripts/updateAppConfig.sh
+++ .github/scripts/updateAppConfig.sh
@@ -3,4 +3,4 @@ configInput=$(cat ./templates/.github/scripts/$1)
 echo ">> $1"
 echo ">> $2"
 echo ">> $configInput"
-curl -X PUT -H "Authorization: Bearer $token" -H "Content-type: application/json" https://management.azure.com/$2?api-version=2019-08-01 --data "$configInput"
+curl -X PATCH -H "Authorization: Bearer $token" -H "Content-type: application/json" https://management.azure.com/$2?api-version=2019-08-01 --data "$configInput"
