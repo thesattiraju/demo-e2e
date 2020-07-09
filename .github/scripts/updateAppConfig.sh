d5c9b9fdc942a3234368ce1f0519d93e9fe2096e
raghaves <raghaves@microsoft.com>
2020-07-09 17:36:21 +0530

    Policy Integration changes

---
+++ .github/scripts/updateAppConfig.sh
@@ -0,0 +1,3 @@
+token=$(az account get-access-token --resource=https://management.azure.com | jq -r .accessToken)
+configInput=$(cat ./workflow/.github/workflows/scripts/$1)
+curl -X PUT -H "Authorization: Bearer $token" -H "Content-type: application/json" https://management.azure.com/$2?api-version=2019-08-01 --data "$configInput"
